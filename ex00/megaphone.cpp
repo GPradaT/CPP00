7e60593805f6f3e594c96a97fed889df492dc8ba
Guillem Prada Torres <gprada-t@car9s1.42barcelona.com>
2024-07-13 12:43:00 +0200

    ex00 reparado

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,25 +6,26 @@
 /*   By: gprada-t <gprada-t@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 11:49:05 by gprada-t          #+#    #+#             */
-/*   Updated: 2024/07/11 15:30:59 by gprada-t         ###   ########.fr       */
+/*   Updated: 2024/07/13 12:42:09 by gprada-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
-#include <cctype>
+// #include <cctype>
+
+void	printString(std::string str)
+{
+	for (int i = 0; i < (int)str.length(); ++i)
+		std::cout << char(std::toupper(str[i]));
+}
 
 int    main(int argc, char **argv)
 {
-    if (argc > 2) {
-		for (int i = 1; i < argc; i++) {
-			for (size_t j = 0; j < sizeof(argv[i]); j++) {
-				std::cout << std::toupper(argv[i][j]);
-			}
-		}
-		std::cout << std::endl;
-	}
-	else
+	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	for (int i = 1; i < argc; i++)
+		printString(std::string(argv[i]));
+	std::cout << std::endl;
 	return 0;
 }
